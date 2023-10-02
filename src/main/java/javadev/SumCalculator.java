2e229f83f8a68c5c7ed0e6dbea7943dd9376bef2
odin0960 <121524275+odin0960@users.noreply.github.com>
2023-10-02 15:17:40 +0300

    some changes

--- src/main/java/javadev/SumCalculator.java
+++ src/main/java/javadev/SumCalculator.java
@@ -3,11 +3,9 @@ package javadev;
 public class SumCalculator {
 
     public int sum(int n) {
-
         if (n <= 0) {
             throw new IllegalArgumentException("Please, input number > 0");
         }
-
         if (n == 1) return 1;
 
         return n + sum(n - 1);
